9221f34bcd28bce8da43a399b18342f3741ba981
eduardobetim <edubetimso@gmail.com>
2020-12-13 20:14:11 -0300

    a

--- sprites.py
+++ sprites.py
@@ -335,20 +335,10 @@ class Item(pg.sprite.Sprite):
         self.type = type
         self.image = self.create_image()
         self.rect = self.image.get_rect()
-        # self.type = type
         self.pos = pos
         self.rect.center = pos
-        #self.tween = tween.easeInOutSine
         self.step = 0
         self.dir = 1
-
-    # def update(self):
-    #     #offset = BOB_RANGE * (self.tween(self.step / BOB_RANGE) - 0.5)
-    #     self.rect.centery = self.pos.y + self.dir
-    #     self.step += BOB_SPEED
-    #     if self.step > BOB_RANGE:
-    #         self.step = 0
-    #         self.dir *= -1
 
     def create_image(self):
         item_images = {}
